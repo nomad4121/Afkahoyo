1392a24f2c9a6c4f6c4ea9e5ca1b18219700b937
nomad4121 <abdihji@gmail.com>
2025-07-17 18:30:22 +0300

    Update and rename next.config.mjs to next.config.js

--- apps/librelingo-web/next.config.mjs
+++
@@ -1,7 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  output: 'standalone', // This is important for dynamic Next.js hosting
-};
-
-module.exports = nextConfig;
